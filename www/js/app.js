0a6db4a31f17c6787c7e61171eda551846629975
quentinchap <chapelle.quentin@gmail.com>
2016-06-07 18:49:31 +0200

    Init base design

--- www/js/app.js
+++ www/js/app.js
@@ -6,40 +6,49 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'firebase', 'starter.sensors'])
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
-  })
-
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-      })
-      .state('app.sensors', {
-        url: '/sensors',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/sensors.html',
-            controller: 'SensorsCtrl'
-          }
-        }
-      });
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/sensors');
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
   });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  .state('app.home',{
+    url: '/home',
+    views:{
+      'menuContent': {
+        templateUrl: 'templates/home.html'
+      }
+    }
+  }
+)
+.state('app.sensors', {
+  url: '/sensors',
+  views: {
+    'menuContent': {
+      templateUrl: 'templates/sensors.html',
+      controller: 'SensorsCtrl'
+    }
+  }
+});
+// if none of the above states are matched, use this as the fallback
+$urlRouterProvider.otherwise('/app/sensors');
+});
---
+++ www/js/challenge.service.js
@@ -0,0 +1,4 @@
+angular.module('starter.service', [])
+.factory('challengeService', function() {
+
+})
