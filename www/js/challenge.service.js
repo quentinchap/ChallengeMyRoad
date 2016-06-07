2a47e1ba6fccfb13b566e25705fe9234f31cb5b8
quentinchap <chapelle.quentin@gmail.com>
2016-06-07 21:40:27 +0200

    design

--- www/js/challenge.service.js
+++ www/js/challenge.service.js
@@ -1,4 +1,15 @@
 angular.module('starter.service', [])
 .factory('challengeService', function() {
 
+  var service = {
+    getThreeRightChallenge:getThreeRightChallenge
+  }
+
+  return service;
+
+  function getThreeRightChallenge()
+  {
+    return { title: "challenge 1"}
+  }
+
 })
