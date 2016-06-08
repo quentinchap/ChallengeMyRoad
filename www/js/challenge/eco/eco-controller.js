8dfb87aea78350ff0638178a865bae5ea03584b4
Jeydu63 <i6znpq96>
2016-06-08 11:42:22 +0200

    add eco challenge

---
+++ www/js/challenge/eco/eco-controller.js
@@ -0,0 +1,23 @@
+angular.module('starter.challenges', [])
+    .controller('ChallengesCtrl', ChallengesCtrl);
+
+ChallengesCtrl.$inject = ['$stateParams'/*'challengesService'*/]
+function ChallengesCtrl($stateParams/*challengesService*/) {
+    var vm = this;
+
+    //vm.challenge = challengesService.read($stateParams.challengeId);
+
+    vm.challenge = {
+        id: $stateParams.challengeId,
+        title: 'Anticiper les obstacles',
+        detail: 'Afin de mieux anticiper les obstacles, vous devez réduire les freinages brusques de 10%.',
+        progress: 8
+    };
+}
+
+
+function ecoChallenge() {
+  return {
+    templateUrl: 'template/eco-challenge.html'
+  };
+};
