6277652c2a4b58aea26f9a0a24d22744f76036f4
Jeydu63 <i6znpq96>
2016-06-08 13:24:55 +0200

    add login screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,85 +1,10 @@
 angular.module('starter.controllers', [])
 
   .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $firebaseArray, $mdSidenav) {
-    var ref = firebase.database().ref().child("bookItems");
+    var ref = firebase.database().ref().child("challenge");
     $scope.messages = $firebaseArray(ref);
-
-    // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-      scope: $scope
-    }).then(function (modal) {
-      $scope.modal = modal;
-    });
-
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function () {
-      $scope.modal.hide();
-    };
-
-    // Open the login modal
-    $scope.login = function () {
-      $scope.modal.show();
-    };
 
     $scope.toggleSidenav = function (menuId) {
       $mdSidenav(menuId).toggle();
-    };
-
-    // Perform the login action when the user submits the login form
-    $scope.loginFacebook = function () {
-      var provider = new firebase.auth.FacebookAuthProvider();
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-
-        localStorage.token = token;
-        localStorage.user = user;
-
-        console.log('Login Facebook Successfully');
-        $scope.closeLogin();
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        console.log("Error : " + errorCode + " - " + errorMessage);
-      });
-    };
-
-    $scope.loginGoogle = function () {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        localStorage.token = token;
-        localStorage.user = user;
-
-        console.log('Login Google Successfully');
-        $scope.closeLogin();
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        console.log("Error : " + errorCode + " - " + errorMessage);
-      });
-    };
-
-    $scope.logout = function () {
-      firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }, function(error) {
-        // An error happened.
-      });
     };
   });
---
+++ www/js/login/LoginCtrl.js
@@ -0,0 +1,67 @@
+angular.module('starter.controllers', []).controller('LoginCtrl', LoginCtrl);
+
+function LoginCtrl($scope, $state) {
+  var vm = this;
+  if(localStorage.user != null) {
+    $state.go('app.sensors');
+  }
+
+  // Perform the login action when the user submits the login form
+  $scope.loginFacebook = function () {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+      localStorage.token = token;
+      localStorage.user = user;
+
+      console.log('Login Facebook Successfully');
+      $state.go('app.sensors');
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log("Error : " + errorCode + " - " + errorMessage);
+    });
+  };
+
+  $scope.loginGoogle = function () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      localStorage.token = token;
+      localStorage.user = user;
+
+      console.log('Login Google Successfully');
+      $state.go('app.sensors');
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log("Error : " + errorCode + " - " + errorMessage);
+    });
+  };
+
+  $scope.logout = function () {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }, function(error) {
+      // An error happened.
+    });
+  };
+}
+
