09352e689abd101fe9339f1c3579c9f8eb7feef2
Benjamin Caure <bcaure@gmail.com>
2016-06-08 17:04:34 +0200

    fix firebase error

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,8 @@
 angular.module('starter.controllers', [])
 
   .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $firebaseArray, $mdSidenav) {
-    var ref = firebase.database().ref().child("challenge");
-    $scope.messages = $firebaseArray(ref);
+    //var ref = firebase.database().ref().child("challenge");
+    //$scope.messages = $firebaseArray(ref);
 
     $scope.toggleSidenav = function (menuId) {
       $mdSidenav(menuId).toggle();
