bf486c2d183652fca61a529e82a2e0830ba1ab03
quentinchap <chapelle.quentin@gmail.com>
2016-06-08 18:18:00 +0200

    bar color + login

--- www/js/progress-bars/progress-bars-directive.js
+++ www/js/progress-bars/progress-bars-directive.js
@@ -1,6 +1,23 @@
 angular.module('starter.progress-bars')
     .directive('ecProgressBars', function () {
         return {
-            templateUrl: './js/progress-bars/progress-bars.tmpl.html'
+            templateUrl: './js/progress-bars/progress-bars.tmpl.html',
+            controller: 'progressBarCtrl'
         };
+    }).controller('progressBarCtrl',function ($scope) {
+
+        $scope.getColor = getColor;
+
+        function getColor(val) {
+          if(val <= 33)
+          {
+            return 'rouge';
+          }
+          else if( val >= 66) {
+            return 'vert';
+          }
+          else {
+            return 'orange';
+          }
+        }
     });
